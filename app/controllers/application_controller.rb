b743b0168694c35fc5d13c8ecf008199caa8fead
Jeremy Bell <jeremy.bell91@gmail.com>
2020-11-12 16:47:55 +1000

    Add sign_up_parameters method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :sign_up_parameters, if: :devise_controller?
+
+    def sign_up_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name)}
+      
+    #   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :first_name, :last_name)}
+    end
+
 end
