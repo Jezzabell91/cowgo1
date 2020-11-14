08d526692726aba2bfe45dc4f13cb4093bfb8e7b
Jeremy Bell <jeremy.bell91@gmail.com>
2020-11-14 10:35:36 +1000

    Added user controller and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/profile'
+
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
   get 'home/index'
+  resources :users
+  get 'users/profile/:id', to: 'users#profile', as: "profile"
+  get 'users/profile/:id/edit', to: 'users#edit_profile', as: "edit_profile"
+  get 'users/profile/:id/create', to: 'users#create_profile', as: "create_profile"
+  post 'users/profile/:id/create', to: 'users#create_profile'
 
   root to: 'home#index'
 end
