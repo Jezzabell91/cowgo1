108d7655ad217e67fbf650302a34ebb5fe579f29
Jeremy Bell <jeremy.bell91@gmail.com>
2020-11-14 17:20:25 +1000

    Added Address model as polymorphic association

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
+
   
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
 
   get 'home/index'
-  resources :users
+
+  resources :users do 
+    resources :addresses, module: :users 
+  end
+
   get 'users/profile/:id', to: 'users#profile', as: "profile"
   get 'users/profile/:id/edit', to: 'users#edit_profile', as: "edit_profile"
   get 'users/profile/:id/create', to: 'users#create_profile', as: "create_profile"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_013944) do
+ActiveRecord::Schema.define(version: 2020_11_14_071034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,19 @@ ActiveRecord::Schema.define(version: 2020_11_12_013944) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "addressable_type", null: false
+    t.bigint "addressable_id", null: false
+    t.string "street_number"
+    t.string "street_name"
+    t.string "state"
+    t.string "postcode"
+    t.string "country", default: "Australia"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable_type_and_addressable_id"
   end
 
   create_table "users", force: :cascade do |t|
